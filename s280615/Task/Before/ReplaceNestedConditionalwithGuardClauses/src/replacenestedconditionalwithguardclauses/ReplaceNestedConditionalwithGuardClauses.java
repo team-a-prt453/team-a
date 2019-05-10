99ecdc490164b9d0cac9d6a504fe8633f26f3894
jbenesha <joel.b13@hotmail.com>
2019-05-10 12:06:43 +0930

    sprint3

---
+++ s280615/Task/Before/ReplaceNestedConditionalwithGuardClauses/src/replacenestedconditionalwithguardclauses/ReplaceNestedConditionalwithGuardClauses.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package replacenestedconditionalwithguardclauses;
+
+import static jdk.nashorn.internal.objects.NativeJava.type;
+
+/**
+ *
+ * @author joelb
+ */
+public class ReplaceNestedConditionalwithGuardClauses {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // the nested conditional before state 
+    //difficult to determine the normal flow of code execution
+    
+    
+  String type = region.getType();
+   if (type == Region.casuarina) {
+      return postcode;
+   } else if (type == Region.nightcliff) {
+      return postcode;
+   } else if (type == Region.Nakarra) {
+      return postcode;
+   } else if (type == Region.Wanguri) {
+      return postcode;
+   } else {
+      return null;
+   }
+} 
+   
